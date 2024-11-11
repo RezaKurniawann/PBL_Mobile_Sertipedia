de39a60cf3d23330ec306b5b8face758f2f20a88
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-11-11 20:02:39 +0700

    input pelatihan & sertifikasi (sidebar)

--- lib/Views/input_pelatihan.dart
+++ lib/Views/input_pelatihan.dart
@@ -2,6 +2,7 @@ import 'dart:convert'; // For base64 encoding
 // import 'dart:typed_data'; // For Uint8List
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+// import 'package:dio/dio.dart';
 
 // void main() {
 //   runApp(const MyApp());
@@ -135,32 +136,172 @@ class _InputPelatihanState extends State<InputPelatihan> {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: const Color(0xFF0B2F9F),
-      leading: Builder(
-        builder: (context) => IconButton(
-          icon: const Icon(Icons.menu),
-          color: Colors.white,
-          onPressed: () => Scaffold.of(context).openDrawer(),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0B2F9F),
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu),
+            color: Colors.white,
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ),
+        titleSpacing: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: const [
+            Text('SERTIPEDIA',
+                style: TextStyle(
+                    fontWeight: FontWeight.w900, color: Colors.white)),
+            Padding(padding: EdgeInsets.only(right: 17.5)),
+          ],
         ),
       ),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const Text(
-            'SERTIPEDIA',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 20,
-            ),
+      drawer: Drawer(
+        child: Container(
+          color: const Color(0xFF0B2F9F),
+          child: ListView(
+            padding: const EdgeInsets.only(top: 0),
+            children: <Widget>[
+              SizedBox(
+                height: 89,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF0B2F9F),
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: IconButton(
+                        icon: const Icon(Icons.menu, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home, color: Colors.white),
+                title:
+                    const Text('Home', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/homepage');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.person, color: Colors.white),
+                title: const Text('Profile',
+                    style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.bar_chart, color: Colors.white),
+                title: const Text('Statistik',
+                    style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/statistik');
+                },
+              ),
+              ExpansionTile(
+                leading: const Icon(Icons.check_circle, color: Colors.white),
+                title: const Text('Verifikasi',
+                    style: TextStyle(color: Colors.white)),
+                children: [
+                  ListTile(
+                    title: const Text('Sertifikasi',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/verifikasi_sertifikasi');
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Pelatihan',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/verifikasi_pelatihan');
+                    },
+                  ),
+                ],
+              ),
+              ListTile(
+                leading: const Icon(Icons.school, color: Colors.white),
+                title: const Text('Kompetensi Prodi',
+                    style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/kompetensi_prodi');
+                },
+              ),
+              ExpansionTile(
+                leading:
+                    const Icon(Icons.workspace_premium, color: Colors.white),
+                title: const Text('Input Data',
+                    style: TextStyle(color: Colors.white)),
+                children: [
+                  ListTile(
+                    title: const Text('Sertifikasi',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/input_sertifikasi');
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Pelatihan',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/input_pelatihan');
+                    },
+                  ),
+                ],
+              ),
+              ListTile(
+                leading:
+                    const Icon(Icons.notifications_active, color: Colors.white),
+                title: const Text('Notifikasi',
+                    style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/notifikasi');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.file_download,
+                    color: Colors.white), // Icon for Download Surat
+                title: const Text('Download Surat',
+                    style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(
+                      context, '/surat_tugas'); // Navigate to Login on logout
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout, color: Colors.white),
+                title:
+                    const Text('Logout', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(
+                      context, '/login'); // Navigate to Login on logout
+                },
+              ),
+            ],
           ),
-          const SizedBox(width: 20), // Optional padding for spacing
-        ],
+        ),
       ),
-    ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
